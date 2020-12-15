fca61d94070a095151c672cdae7a39fed6ea7b8d
Otávio Augusto Dellay Secco <otavio.secco@estudante.uffs.edu.br>
2020-12-15 14:16:04 -0300

    Fixed some parameters

--- HDPmodel.py
+++ HDPmodel.py
@@ -1,14 +1,14 @@
 !pip install tomotopy
 import tomotopy as tp
 
-# Train NMF model.
+# Train HDP model.
 
 mdl = tp.HDPModel(min_cf=0, min_df=0, rm_top=0, initial_k=30, alpha=1, eta=0.01, gamma=1)
 for line in open('bigrammed.txt'):
     mdl.add_doc(line.strip().split())
 
-for i in range(0, 1000, 100):
-    mdl.train(100)
+for i in range(0, 1000, 10):
+    mdl.train(10)
     print('Iteration: {}\tLog-likelihood: {}'.format(i, mdl.ll_per_word))
 
 for k in range(mdl.k):
